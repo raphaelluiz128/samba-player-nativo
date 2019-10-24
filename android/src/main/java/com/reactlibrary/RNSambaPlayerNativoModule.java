8a4dae7be97a757c601a48ec62289bad1e1b994a
RAPHAEL LUIZ SCHETTINO <raphaelluiz128@hotmail.com>
2019-10-23 22:24:59 -0300

    Add files via upload

---
+++ android/src/main/java/com/reactlibrary/RNSambaPlayerNativoModule.java
@@ -0,0 +1,28 @@
+
+package com.reactlibrary;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+public class RNSambaPlayerNativoModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+
+  public RNSambaPlayerNativoModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNSambaPlayerNativo";
+  }
+
+  @ReactMethod
+public void checkBridge(final Promise promise) {
+  promise.resolve(“Funcionou!”);
+}
+
+}
